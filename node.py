72e29c6a051915f918887935e9054deecfc096c4
geraJuarez <geracjuarez@gmail.com>
2019-01-29 20:07:56 -0600

    node structure tested

--- node.py
+++ node.py
@@ -1,38 +1,70 @@
 class Node:
+    # The board atribute is the array of integers that represents the state of the puzzle
+    # [0, 1, 2
+    #  3, 4, 5
+    #  6, 7, 8]
     def __init__(self, arr):
         self.board = arr
         self.spaceIndex = 0
         for i in range(len(arr)):
             if (arr[i] == 0):
                 self.spaceIndex = i
-
-    def __str__(self): # string
-        return self.board
+        self.__verticalDistance = 3 # Distance to simulate moving the space of the puzzle up or down
 
     def __repr__(self): # in a list or other structure
         return self.board
 
+    def __onUpperBound(self, index):
+        return index in range(0, 3)
+
+    def __onLowerBound(self, index):
+        return index in range(6, 9)
+
+    def __onLeftBound(self, index):
+        return index == 0 or index == 3 or index == 6
+
+    def __onRightBound(self, index):
+        return index == 2 or index == 5 or index == 8
+
+    def __swapValues(self, newIndex):
+        self.board[self.spaceIndex] = self.board[newIndex]
+        self.board[newIndex] = 0
+        self.spaceIndex = newIndex
+
     def up(self):
-        if self.spaceIndex in range(0, 3):
+        if self.__onUpperBound(self.spaceIndex):
             return None  
         else:
-            pass
+            self.__swapValues(self.spaceIndex - self.__verticalDistance)
+            return self.board
     
     def down(self):
-        if self.spaceIndex in range(6, 9):
+        if self.__onLowerBound(self.spaceIndex):
             return None  
         else:
-            pass
+            self.__swapValues(self.spaceIndex + self.__verticalDistance)
+            return self.board
 
     def left(self):
-        if self.spaceIndex == 0 or self.spaceIndex == 3 or self.spaceIndex == 6:
+        if self.__onLeftBound(self.spaceIndex):
             return None  
         else:
-            pass
+            self.__swapValues(self.spaceIndex - 1)
+            return self.board
 
     def right(self):
-        self.spaceIndex == 2 or self.spaceIndex == 5 or self.spaceIndex == 8:
+        if self.__onRightBound(self.spaceIndex):
             return None
         else:
-            pass
+            self.__swapValues(self.spaceIndex + 1)
+            return self.board
     
+def debugMethods():
+    x = Node([0, 1, 2, 3, 4, 5, 6 ,7 ,8])
+    print(x.right())
+    print(x.down())
+    print(x.left())
+    print(x.up())
+
+if __name__ == "__main__":
+    debugMethods()
