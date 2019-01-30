3aa3c1d9dd6fbb467deaffcef9e94b6e20f9a30c
geraJuarez <geracjuarez@gmail.com>
2019-01-29 20:29:29 -0600

    Add method to retrieve all posible paths of the puzzle state

--- node.py
+++ node.py
@@ -1,18 +1,15 @@
 class Node:
-    # The board atribute is the array of integers that represents the state of the puzzle
+    # The puzzleState atribute is the array of integers that represents the state of the puzzle
     # [0, 1, 2
     #  3, 4, 5
     #  6, 7, 8]
     def __init__(self, arr):
-        self.board = arr
-        self.spaceIndex = 0
+        self.puzzleState = arr
+        self.spaceIndex = 0         # index where the zero value is. The space of the puzzle
         for i in range(len(arr)):
             if (arr[i] == 0):
                 self.spaceIndex = i
         self.__verticalDistance = 3 # Distance to simulate moving the space of the puzzle up or down
-
-    def __repr__(self): # in a list or other structure
-        return self.board
 
     def __onUpperBound(self, index):
         return index in range(0, 3)
@@ -26,38 +23,55 @@ class Node:
     def __onRightBound(self, index):
         return index == 2 or index == 5 or index == 8
 
-    def __swapValues(self, newIndex):
-        self.board[self.spaceIndex] = self.board[newIndex]
-        self.board[newIndex] = 0
-        self.spaceIndex = newIndex
+    def __moveSpace(self, newIndex):
+        newState = self.puzzleState.copy()
+        newState[self.spaceIndex] = newState[newIndex]
+        newState[newIndex] = 0
+        return newState
 
     def up(self):
         if self.__onUpperBound(self.spaceIndex):
             return None  
         else:
-            self.__swapValues(self.spaceIndex - self.__verticalDistance)
-            return self.board
+            return self.__moveSpace(self.spaceIndex - self.__verticalDistance)
     
     def down(self):
         if self.__onLowerBound(self.spaceIndex):
             return None  
         else:
-            self.__swapValues(self.spaceIndex + self.__verticalDistance)
-            return self.board
+            return self.__moveSpace(self.spaceIndex + self.__verticalDistance)
 
     def left(self):
         if self.__onLeftBound(self.spaceIndex):
             return None  
         else:
-            self.__swapValues(self.spaceIndex - 1)
-            return self.board
+            return self.__moveSpace(self.spaceIndex - 1)
 
     def right(self):
         if self.__onRightBound(self.spaceIndex):
             return None
         else:
-            self.__swapValues(self.spaceIndex + 1)
-            return self.board
+            return self.__moveSpace(self.spaceIndex + 1)
+
+    def posiblePaths(self):
+        paths = []
+        state = self.up()
+        if (state != None):
+            paths.append(state)
+        
+        state = self.down()
+        if (state != None):
+            paths.append(state)
+
+        state = self.left()
+        if (state != None):
+            paths.append(state)
+
+        state = self.right()
+        if (state != None):
+            paths.append(state)
+        
+        return paths
     
 def debugMethods():
     x = Node([0, 1, 2, 3, 4, 5, 6 ,7 ,8])
@@ -65,6 +79,8 @@ def debugMethods():
     print(x.down())
     print(x.left())
     print(x.up())
+    print("_________")
+    print(x.posiblePaths())
 
 if __name__ == "__main__":
     debugMethods()
