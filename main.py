e5bd37ecc2baf7e7eabf1d3488ae62f763947915
geraJuarez <geracjuarez@gmail.com>
2019-01-30 18:05:43 -0600

    read from file

--- main.py
+++ main.py
@@ -52,10 +52,13 @@ def breadthFirstSearch(initialState, finalState):
     return None
 
 def main():
-    #with open("hola.txt", "r") as file:
-
-    initialState = [1,2,5,3,4,0,6,7,8] # read from textfile
+    initialState = []
     goal = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+    with open("puzzle.txt", "r") as file:
+        line = file.readline()
+        for val in line.split(" "):
+            initialState.append(int(val))
     
     start = timer()
     finalState, exploredSet, size = breadthFirstSearch(initialState, goal)
