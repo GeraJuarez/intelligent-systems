ea2d2b87785bc84e2fc5b217a63eecb4e3f4b065
geraJuarez <geracjuarez@gmail.com>
2019-02-02 23:45:05 -0600

    more python convention names

--- 8-puzzle/a-star/main.py
+++ 8-puzzle/a-star/main.py
@@ -78,28 +78,28 @@ def a_start(initial_state, final_state):
     return None, None, None
 
 def main(arg):
-    initialState = []
+    initial_state = []
     goal = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
     with open(arg, "r") as file:
         line = file.readline()
         for val in line.split(" "):
-            initialState.append(int(val))
+            initial_state.append(int(val))
 
-    if set(goal) != set(initialState):
+    if set(goal) != set(initial_state):
         sys.exit("ERROR: input is not valid, it must have numbers from 0 to 8")
-    elif len(goal) != len(initialState):
+    elif len(goal) != len(initial_state):
         sys.exit("ERROR: invalid input, it must have nine numbers, from 0 to 8")
     
     start = timer()
-    finalState, exploredSet, size = a_start(initialState, goal)
+    final_state, explored_set, size = a_start(initial_state, goal)
     end = timer()
     
-    if finalState != None:
+    if final_state != None:
         print("=" * 20)
-        print("Solution depth or cost: %d" % len(finalState.path))
+        print("Solution depth or cost: %d" % len(final_state.path))
         print("=" * 20)
-        print("Number of visited nodes: %d" % len(exploredSet))
+        print("Number of visited nodes: %d" % len(explored_set))
         print("=" * 20)
         print("Running time: %f sec" % (end - start))
         print("=" * 20)
@@ -108,13 +108,14 @@ def main(arg):
         print("=" * 20)
         print("This are the movements to solve the puzzle:")
         print("-" * 20)
-        print(*finalState.path, sep=' => ')
+        print(*final_state.path, sep=' => ')
     else:
         print("It has no solution :(")
 
-if len(sys.argv) < 2:
-    sys.exit("ERROR: The program needs the name of the file with the initial state of the puzzle as an argument when executing")
-elif len(sys.argv) > 2:
-    sys.exit("ERROR: too many arguments")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit("ERROR: The program needs the name of the file with the initial state of the puzzle as an argument when executing")
+    elif len(sys.argv) > 2:
+        sys.exit("ERROR: too many arguments")
 
-main(sys.argv[1])
+    main(sys.argv[1])
